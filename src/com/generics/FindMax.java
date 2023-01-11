860a1683f4150e71e4e1b2a49c101b8607224cce
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2023-01-12 00:16:50 +0530

    Given 3 integer find the maximum

--- src/com/generics/FindMax.java
+++ src/com/generics/FindMax.java
@@ -2,9 +2,36 @@ package com.generics;
 
 import java.util.Scanner;
 
-/*Given 3 Floats find the maximum*/
+/*Given 3 integer find the maximum*/
 public class FindMax {
-    public static void main(String[] args) {
-        System.out.println("Welcome to find max using generic");
+
+    public static void compareTo(Integer a, Integer b, Integer c) {
+        int max = a;
+
+        if (b.compareTo(max)> 0) {
+            max = b;
+
+            if (c.compareTo(max) > 0)
+                max = c;
+
+            System.out.println("The maximum number is:" + max);
+        }
     }
+
+    public static void main(String[] args) {
+
+        Integer a, b, c;
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        a = sn.nextInt();
+        System.out.println("Enter the value of b");
+         b = sn.nextInt();
+        System.out.println("Enter the value of c");
+        c = sn.nextInt();
+
+        compareTo(a, b, c);
+
+    }
+
 }
