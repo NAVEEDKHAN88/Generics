fa1981abc697a24fbd15e300666f54305d06d6eb
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2023-01-12 00:56:20 +0530

    find maximum string among three strings using findMax method

--- src/com/generics/FindMax.java
+++ src/com/generics/FindMax.java
@@ -2,14 +2,15 @@ package com.generics;
 
 import java.util.Scanner;
 
-/*Given 3 Float find the maximum*/
+/*Purpose to find maximum string among three strings using findMax method*/
 public class FindMax {
 
-    public static void compareTo(Float a, Float b, Float c) {
-        Float max = a;
+    public static void compareTo(String a, String b, String c) {
+        String max = a;
 
         if (b.compareTo(max)> 0) {
             max = b;
+        }
 
             if (c.compareTo(max) > 0) {
                 max = c;
@@ -18,21 +19,21 @@ public class FindMax {
 
             System.out.print("The maximum number is:" + max+"\n");
         }
-    }
+
 
     public static void main(String[] args) {
 
        while (true){
 
-           float a, b, c;
+           String a, b, c;
 
         Scanner sn = new Scanner(System.in);
         System.out.println("Enter the value of a");
-        a = sn.nextFloat();
+        a = sn.next();
         System.out.println("Enter the value of b");
-         b = sn.nextFloat();
+         b = sn.next();
         System.out.println("Enter the value of c");
-        c = sn.nextFloat();
+        c = sn.next();
         compareTo(a, b, c);
 
     }
