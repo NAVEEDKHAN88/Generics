8599f3fa6150a44549be0a5d75e3351c4ade37bc
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2023-01-23 14:39:26 +0530

    One Generic Method and find the maximum of string,integer,float

--- src/com/generics/FindMax.java
+++ src/com/generics/FindMax.java
@@ -2,41 +2,51 @@ package com.generics;
 
 import java.util.Scanner;
 
-/*Purpose to find maximum string among three strings using findMax method*/
-public class FindMax {
+/*
+ * Purpose to find maximum string among three strings using findMax method
+ */
+public class FindMax <T extends Comparable<T>>{
+    T a,b,c;
 
-    public static void compareTo(String a, String b, String c) {
-        String max = a;
+    public FindMax(T a, T b, T c) {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    private static <T extends Comparable<T>> void compareTo(T s1, T s2, T s3) {
+        // TODO Auto-generated method stub
+        T max = s1;
 
-        if (b.compareTo(max)> 0) {
-            max = b;
+        if (s2.compareTo(max) > 0) {
+            max = s2;
         }
-
-            if (c.compareTo(max) > 0) {
-                max = c;
-
-            }
-
-            System.out.print("The maximum number is:" + max+"\n");
+        if (s3.compareTo(max) > 0) {
+            max = s3;
         }
-
-
-    public static void main(String[] args) {
-
-       while (true){
-
-           String a, b, c;
-
-        Scanner sn = new Scanner(System.in);
-        System.out.println("Enter the value of a");
-        a = sn.next();
-        System.out.println("Enter the value of b");
-         b = sn.next();
-        System.out.println("Enter the value of c");
-        c = sn.next();
-        compareTo(a, b, c);
-
+        System.out.println("The maximum among three is : " + max);
     }
 
-}
+    public static void main(String[] args) {
+        Integer a, b, c;
+        Float F1, F2, F3;
+        String S1, S2, S3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the three integers");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        compareTo(a, b, c);
+        System.out.println("Enter the three floats");
+        F1 = sc.nextFloat();
+        F2 = sc.nextFloat();
+        F3 = sc.nextFloat();
+        compareTo(F1, F2, F3);
+        System.out.println("Enter the three strings");
+        S1 = sc.next();
+        S2 = sc.next();
+        S3 = sc.next();
+        compareTo(S1, S2, S3);
+        sc.close();
+    }
+
 }
